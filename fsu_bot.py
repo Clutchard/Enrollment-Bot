bc6ed38b87e3f91cc850514e869769304088dd4d
Clutchard <rrn14@my.fsu.edu>
2017-07-29 21:46:14 -0400

    Add files via upload

--- fsu_bot.py
+++ fsu_bot.py
@@ -12,6 +12,17 @@ def get_credentials():
 	password = getpass.getpass("Enter your password: ")
 	return username, password
 
+def get_class_number():
+	x = True
+	class_number = raw_input("\nNow enter the numercial class number: ")
+	if (class_number.isdigit()):
+			x = False
+			while x == True:
+				class_number = raw_input("\nPlease try again: ")
+				if (class_number.isdigit()):
+					x = False
+	return class_number
+
 
 def login_function(driver, username, password):
 	driver.get("https://cas.fsu.edu/cas/login?service=https%3A%2F%2Fwww.my.fsu.edu%2Fc%2Fportal%2Flogin")
@@ -25,11 +36,26 @@ def login_function(driver, username, password):
 	driver.find_element_by_id("link_icon_200"))
 
 def student_central(driver):
-	student_center_button = WebDriverWait(driver, 5).until(lambda driver: 
-	driver.find_element_by_id("link_icon_200"))
-	student_center_button.click()
-	enroll_button = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_id("DERIVED_SSS_SCR_SSS_LINK_ANCHOR3$span"))
-	enroll_button.click()
+    student_center_button = WebDriverWait(driver, 5).until(lambda driver: 
+    driver.find_element_by_id("link_icon_200"))
+    student_center_button.click()
+    frame = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_xpath("""//*[@id="ptifrmtgtframe"]"""))
+    driver.switch_to.frame(frame)
+    enroll_button = driver.find_element_by_id("DERIVED_SSS_SCR_SSS_LINK_ANCHOR3")
+    enroll_button.click()
+    num_field = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_id("DERIVED_REGFRM1_CLASS_NBR"))
+
+def class_number_search(driver, course_number):
+	driver.find_element_by_id("DERIVED_REGFRM1_CLASS_NBR").clear()
+	num_field = driver.find_element_by_id("DERIVED_REGFRM1_CLASS_NBR")
+	num_field.send_keys(course_number)
+	enter_button = driver.find_element_by_id("DERIVED_REGFRM1_SSR_PB_ADDTOLIST2$9$")
+	enter_button.click()
+	next_button = WebDriverWait(driver, 5).until(lambda driver: driver.find_element_by_id("DERIVED_CLS_DTL_NEXT_PB"))
+	next_button.click()
+
+
+
 
 
 if __name__ == '__main__':
@@ -50,15 +76,10 @@ if __name__ == '__main__':
 
 	#Choice 1 has error check to make sure its a 4 digit number
 	choice = raw_input("Please enter your choice: ")
+	class_number = ""
 	if choice == '1':
-		x = True
-		class_number = raw_input("\nNow enter the 4 digit class number: ")
-		if (class_number.isdigit()):
-			x = False
-		while x == True:
-			class_number = raw_input("\nPlease try again: ")
-			if (class_number.isdigit()):
-				x = False
+		temp = get_class_number()
+		class_number = temp
 	else:
 		course_name = raw_input("Please enter the course name as it appears in student central: ")
 		section_number = raw_input("Please enter the section number: ")
@@ -87,12 +108,18 @@ if __name__ == '__main__':
 			print("Contact Support")
 			sys.exit()
 	#Error occurs if can't locate the enroll button on the newly loaded page
-	
-	#comment
-
 
 	if choice == '1':
-		print("Choice 1")
+		while True: 
+			try:
+				class_number_search(driver, class_number)
+				break
+				#trys to login to the fsu website
+			except TimeoutException:
+				print("\nIncorrect Class Number\nTry Again\n")
+				class_number = get_class_number()
+
+
 	else:
 		print("Choice 2")
 
